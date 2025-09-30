4dfc0418d728869d92b17124f8ffb42664706192
monkeyman192 <monkey_man_192@yahoo.com.au>
2025-09-30 13:21:10 +1000

    Fix integration test

--- tests/integration/test_app.py
+++ tests/integration/test_app.py
@@ -1,7 +1,6 @@
 import os
 import os.path as op
 import tempfile
-import unittest
 
 import pymhf
 
@@ -9,31 +8,26 @@ MODS_DIR = op.realpath(op.join(op.dirname(__file__), "..", "test_mods"))
 APPS_DIR = op.realpath(op.join(op.dirname(__file__), "..", "programs"))
 
 
-class MyTest(unittest.TestCase):
-    def test_default_args(self):
-        with tempfile.TemporaryDirectory() as tempdir:
-            config = {
-                "exe": op.join(APPS_DIR, "app.exe"),
-                "start_paused": True,
-                "start_exe": True,
-                "interactive_console": False,
-                "logging": {"log_dir": tempdir},
-            }
-            pymhf.main.REMOVE_SELF = False
-            pymhf.load_mod_file(op.join(MODS_DIR, "app_test_mod.py"), config)
+def test_default_args():
+    with tempfile.TemporaryDirectory() as tempdir:
+        config = {
+            "exe": op.join(APPS_DIR, "app.exe"),
+            "start_paused": True,
+            "start_exe": True,
+            "interactive_console": False,
+            "logging": {"log_dir": tempdir},
+        }
+        pymhf.main.REMOVE_SELF = False
+        pymhf.load_mod_file(op.join(MODS_DIR, "app_test_mod.py"), config)
 
-            log_files = os.listdir(tempdir)
-            self.assertEquals(len(log_files), 1)
-            log_file = log_files[0]
-            important_lines = []
-            with open(op.join(tempdir, log_file), "r") as f:
-                for line in f:
-                    if "TestLogger" in line:
-                        idx = line.find("INFO")
-                        actual_val = line[idx + 4 :]
-                        important_lines.append(actual_val.strip())
-            self.assertEqual(important_lines, ["Doubling the inputs 2 and 3", "(2 * 2) * (3 * 2) = 24"])
-
-
-if __name__ == "__main__":
-    unittest.main()
+        log_files = os.listdir(tempdir)
+        assert len(log_files) == 1
+        log_file = log_files[0]
+        important_lines = []
+        with open(op.join(tempdir, log_file), "r") as f:
+            for line in f:
+                if "TestLogger" in line:
+                    idx = line.find("INFO")
+                    actual_val = line[idx + 4 :]
+                    important_lines.append(actual_val.strip())
+        assert important_lines == ["Doubling the inputs 2 and 3", "(2 * 2) * (3 * 2) = 24"]
